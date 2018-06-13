3183ae2c8590e30a8cb697c728ab158d3a0d4c5c
fperez <c_fperez@hyphensolutions.com>
2018-06-13 17:01:39 -0500

    first Automation test

---
+++ Hyphen AT/Object Repository/Page_hyphen solutions - Google Sear/a_Hyphen Solutions  Constructi.rs
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>a_Hyphen Solutions  Constructi</name>
+   <tag></tag>
+   <elementGuidId>634ad55d-e3f7-4f23-8889-12e6b0e4b279</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>a</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>href</name>
+      <type>Main</type>
+      <value>https://www.hyphensolutions.com/</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>ping</name>
+      <type>Main</type>
+      <value>/url?sa=t&amp;source=web&amp;rct=j&amp;url=https://www.hyphensolutions.com/&amp;ved=0ahUKEwjJgJXXzNHbAhUNZKwKHfNBD7QQFggoMAA</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Hyphen Solutions | Construction Scheduling &amp; Management Software</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;rso&quot;)/div[@class=&quot;bkWMgd&quot;]/div[@class=&quot;g&quot;]/div[1]/div[1]/div[@class=&quot;rc&quot;]/h3[@class=&quot;r&quot;]/a[1]</value>
+   </webElementProperties>
+</WebElementEntity>
---
+++ Hyphen AT/Test Suites/Hyphen AT.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Hyphen AT</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-06-13T16:54:34</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>3f75a887-6392-45a3-82ab-cc886f6b187c</testSuiteGuid>
+   <testCaseLink>
+      <guid>95994233-bec2-4800-bc2e-ab2f95407526</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Open Hyphen Page</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
